aacafd100fbbaab7b6de1d2fc41f30d1f1c5fab3
Ruslan <steiger.ruslan@gmail.com>
2021-03-29 14:07:48 +0200

    wip

--- src/DocuWareFacade.php
+++ src/DocuWareFacade.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class DocuWareFacade extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'laravel-docuware';
     }
---
+++ tests/Feature/DocuWareTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace codebar\DocuWare\Tests\Feature;
+
+use codebar\DocuWare\Tests\TestCase;
+
+class DocuWareTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}

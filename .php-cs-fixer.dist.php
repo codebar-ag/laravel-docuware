a2c7e84f5cfa9c35f2df4b435756e62847d0343a
Ruslan <steiger.ruslan@gmail.com>
2021-06-23 12:17:17 +0200

    🔧 Updated php-cs-fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,17 +5,21 @@ use PhpCsFixer\Finder;
 
 $rules = [
     '@PSR2' => true,
+    'array_indentation' => true,
     'array_syntax' => ['syntax' => 'short'],
     'binary_operator_spaces' => true,
     'blank_line_before_statement' => [
         'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
     ],
-    'cast_spaces' => true,
+    'class_attributes_separation' => [
+        'elements' => ['const' => 'one','method' => 'one','property' => 'one'],
+    ],
     'concat_space' => ['spacing' => 'one'],
     'method_argument_space' => [
         'on_multiline' => 'ensure_fully_multiline',
         'keep_multiple_spaces_after_comma' => true,
     ],
+    'method_chaining_indentation' => true,
     'no_unused_imports' => true,
     'not_operator_with_successor_space' => false,
     'ordered_imports' => ['sort_algorithm' => 'alpha'],

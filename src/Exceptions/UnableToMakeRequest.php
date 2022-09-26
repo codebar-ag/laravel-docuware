630364b629cfa3a47420d64a6aea54ac5a7c8e9e
Sebastian Fix <sebastian.fix@codebar.ch>
2022-09-26 17:31:02 +0200

    WIP

--- src/Exceptions/UnableToMakeRequest.php
+++ src/Exceptions/UnableToMakeRequest.php
@@ -9,15 +9,9 @@ class UnableToMakeRequest extends RuntimeException
     public static function create(): self
     {
         return new static(
-<<<<<<< HEAD
             'You are not authorized. ' .
-                'Make sure you are logged in with the correct credentials.' .
-                'Try to use "->login()" before you make other requests.',
-=======
-            'You are not authorized. '.
-                'Make sure you are logged in with the correct credentials.'.
-                'Try to use "->login()" before you make other requests.'
->>>>>>> feature-update
+            'Make sure you are logged in with the correct credentials.' .
+            'Try to use "->login()" before you make other requests.',
         );
     }
 }

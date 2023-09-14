e9dcee0f858d392595cbaccf9951e73903a4ddbf
RhysLees <RhysLees@users.noreply.github.com>
2023-09-14 21:09:58 +0000

    Fix styling

--- src/DocuWare.php
+++ src/DocuWare.php
@@ -2,41 +2,11 @@
 
 namespace CodebarAg\DocuWare;
 
-use Carbon\Carbon;
-use CodebarAg\DocuWare\Connectors\DocuWareConnector;
-use CodebarAg\DocuWare\DTO\Dialog;
-use CodebarAg\DocuWare\DTO\Document;
-use CodebarAg\DocuWare\DTO\DocumentThumbnail;
-use CodebarAg\DocuWare\DTO\Field;
-use CodebarAg\DocuWare\DTO\FileCabinet;
-use CodebarAg\DocuWare\DTO\Organization;
-use CodebarAg\DocuWare\DTO\OrganizationIndex;
 use CodebarAg\DocuWare\Events\DocuWareResponseLog;
-use CodebarAg\DocuWare\Exceptions\UnableToGetDocumentCount;
 use CodebarAg\DocuWare\Requests\Auth\GetLogoffRequest;
-use CodebarAg\DocuWare\Requests\Document\DeleteDocumentRequest;
-use CodebarAg\DocuWare\Requests\Document\GetDocumentCountRequest;
-use CodebarAg\DocuWare\Requests\Document\GetDocumentDownloadRequest;
-use CodebarAg\DocuWare\Requests\Document\GetDocumentPreviewRequest;
-use CodebarAg\DocuWare\Requests\Document\GetDocumentRequest;
-use CodebarAg\DocuWare\Requests\Document\GetDocumentsDownloadRequest;
-use CodebarAg\DocuWare\Requests\Document\PostDocumentRequest;
-use CodebarAg\DocuWare\Requests\Document\PutDocumentFieldsRequest;
-use CodebarAg\DocuWare\Requests\Document\Thumbnail\GetDocumentDownloadThumbnailRequest;
-use CodebarAg\DocuWare\Requests\GetCabinetsRequest;
-use CodebarAg\DocuWare\Requests\GetDialogsRequest;
-use CodebarAg\DocuWare\Requests\GetFieldsRequest;
-use CodebarAg\DocuWare\Requests\GetSelectListRequest;
-use CodebarAg\DocuWare\Requests\Organization\GetOrganizationRequest;
-use CodebarAg\DocuWare\Requests\Organization\GetOrganizationsRequest;
 use CodebarAg\DocuWare\Support\Auth;
 use CodebarAg\DocuWare\Support\EnsureValidCookie;
 use CodebarAg\DocuWare\Support\EnsureValidCredentials;
-use CodebarAg\DocuWare\Support\EnsureValidResponse;
-use CodebarAg\DocuWare\Support\ParseValue;
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
 use Saloon\Exceptions\InvalidResponseClassException;
 use Saloon\Exceptions\PendingRequestException;
 

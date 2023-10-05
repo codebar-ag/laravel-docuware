5286190af6337a6da8b3a74ff26ce0abb689b679
Sebastian Fix <sebastian.fix@codebar.ch>
2023-10-05 16:41:28 +0200

    WIP

--- src/DTO/Cookie.php
+++ src/DTO/Cookie.php
@@ -9,26 +9,25 @@ use Illuminate\Support\Carbon;
 
 final class Cookie
 {
-	public function __construct(
-		public string $cookie,
-		public Carbon $cookie_created_at,
-		public Carbon $cookie_lifetime_until,
-	)
-	{
-	}
+    public function __construct(
+        public string $cookie,
+        public Carbon $cookie_created_at,
+        public Carbon $cookie_lifetime_until,
+    ) {
+    }
 
-	public static function make(CookieJar $cookieJar): self
-	{
-		throw_if($cookieJar->toArray() === [], UnableToLoginNoCookies::create());
+    public static function make(CookieJar $cookieJar): self
+    {
+        throw_if($cookieJar->toArray() === [], UnableToLoginNoCookies::create());
 
-		$data = collect($cookieJar->toArray())
-			->reject(fn(array $cookie) => Arr::get($cookie, 'Value') === '')
-			->firstWhere('Name', '.DWPLATFORMAUTH');
+        $data = collect($cookieJar->toArray())
+            ->reject(fn (array $cookie) => Arr::get($cookie, 'Value') === '')
+            ->firstWhere('Name', '.DWPLATFORMAUTH');
 
-		return new self(
-			cookie: Arr::get($data, 'Value'),
-			cookie_created_at: now(),
-			cookie_lifetime_until: now()->addMinutes(525600 * 0.75),
-		);
-	}
+        return new self(
+            cookie: Arr::get($data, 'Value'),
+            cookie_created_at: now(),
+            cookie_lifetime_until: now()->addMinutes(525600 * 0.75),
+        );
+    }
 }

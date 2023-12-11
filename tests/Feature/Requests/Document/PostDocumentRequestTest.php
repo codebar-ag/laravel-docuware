7a16a10350f419fc63556fafd299775664485877
Sebastian Fix <sebastian.fix@codebar.ch>
2023-12-11 22:53:21 +0100

    WIP

--- tests/Feature/Requests/Document/PostDocumentRequestTest.php
+++ tests/Feature/Requests/Document/PostDocumentRequestTest.php
@@ -58,3 +58,32 @@ it('can upload document with index values and delete it', function () {
     });
     Event::assertDispatched(DocuWareResponseLog::class);
 });
+
+it('can upload document without file name and file content and delete it', function () {
+    Event::fake();
+
+    $fileCabinetId = config('laravel-docuware.tests.file_cabinet_id');
+
+    $document = $this->connector->send(new PostDocumentRequest(
+        $fileCabinetId,
+        null,
+        null,
+        collect([
+            IndexTextDTO::make('DOCUMENT_LABEL', '::text::'),
+        ]),
+    ))->dto();
+
+    $this->connector->send(new DeleteDocumentRequest(
+        $fileCabinetId,
+        $document->id,
+    ))->dto();
+
+    $this->assertInstanceOf(Document::class, $document);
+
+    tap($document->fields['DOCUMENT_LABEL'], function (DocumentField $field) {
+        $this->assertSame($field->name, 'DOCUMENT_LABEL');
+        $this->assertSame($field->type, 'String');
+        $this->assertSame($field->value, '::text::');
+    });
+    Event::assertDispatched(DocuWareResponseLog::class);
+});

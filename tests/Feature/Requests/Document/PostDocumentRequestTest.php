d97d20422ab9d6910f6d5cfbead7d504ad66563f
Sebastian Fix <sebastian.fix@codebar.ch>
2023-12-11 22:59:07 +0100

    WIP

--- tests/Feature/Requests/Document/PostDocumentRequestTest.php
+++ tests/Feature/Requests/Document/PostDocumentRequestTest.php
@@ -27,6 +27,35 @@ beforeEach(function () {
     $this->connector = new DocuWareStaticConnector($config);
 });
 
+it('can upload document without file name and file content and delete it', function () {
+    Event::fake();
+
+    $fileCabinetId = config('laravel-docuware.tests.file_cabinet_id');
+
+    $document = $this->connector->send(new PostDocumentRequest(
+        $fileCabinetId,
+        null,
+        null,
+        collect([
+            IndexTextDTO::make('DOCUMENT_LABEL', '::data-entry::'),
+        ]),
+    ))->dto();
+
+    $this->connector->send(new DeleteDocumentRequest(
+        $fileCabinetId,
+        $document->id,
+    ))->dto();
+
+    $this->assertInstanceOf(Document::class, $document);
+
+    tap($document->fields['DOCUMENT_LABEL'], function (DocumentField $field) {
+        $this->assertSame($field->name, 'DOCUMENT_LABEL');
+        $this->assertSame($field->type, 'String');
+        $this->assertSame($field->value, '::data-entry::');
+    });
+    Event::assertDispatched(DocuWareResponseLog::class);
+});
+
 it('can upload document with index values and delete it', function () {
     Event::fake();
 
@@ -51,35 +80,6 @@ it('can upload document with index values and delete it', function () {
     $this->assertInstanceOf(Document::class, $document);
 
     $this->assertSame('example', $document->title);
-    tap($document->fields['DOCUMENT_LABEL'], function (DocumentField $field) {
-        $this->assertSame($field->name, 'DOCUMENT_LABEL');
-        $this->assertSame($field->type, 'String');
-        $this->assertSame($field->value, '::text::');
-    });
-    Event::assertDispatched(DocuWareResponseLog::class);
-});
-
-it('can upload document without file name and file content and delete it', function () {
-    Event::fake();
-
-    $fileCabinetId = config('laravel-docuware.tests.file_cabinet_id');
-
-    $document = $this->connector->send(new PostDocumentRequest(
-        $fileCabinetId,
-        null,
-        null,
-        collect([
-            IndexTextDTO::make('DOCUMENT_LABEL', '::text::'),
-        ]),
-    ))->dto();
-
-    $this->connector->send(new DeleteDocumentRequest(
-        $fileCabinetId,
-        $document->id,
-    ))->dto();
-
-    $this->assertInstanceOf(Document::class, $document);
-
     tap($document->fields['DOCUMENT_LABEL'], function (DocumentField $field) {
         $this->assertSame($field->name, 'DOCUMENT_LABEL');
         $this->assertSame($field->type, 'String');

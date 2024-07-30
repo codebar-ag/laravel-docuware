3072e67e992cb725aa5994d5bb8f0cb8a813671d
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2024-07-30 22:28:20 +0100

    WIP

--- config/laravel-docuware.php
+++ config/laravel-docuware.php
@@ -83,28 +83,4 @@ return [
         'client_id' => env('DOCUWARE_CLIENT_ID', 'docuware.platform.net.client'),
         'scope' => env('DOCUWARE_SCOPE', 'docuware.platform'),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Tests
-    |--------------------------------------------------------------------------
-    |
-    */
-    'tests' => [
-        'file_cabinet_id' => env('DOCUWARE_TESTS_FILE_CABINET_ID'),
-        'dialog_id' => env('DOCUWARE_TESTS_DIALOG_ID'),
-        'basket_id' => env('DOCUWARE_TESTS_BASKET_ID'),
-        'section' => (int) env('DOCUWARE_TESTS_SECTION'),
-        'organization_id' => env('DOCUWARE_TESTS_ORGANIZATION_ID'),
-        'document_id' => (int) env('DOCUWARE_TESTS_DOCUMENT_ID'),
-        'document_file_size_preview' => (int) env('DOCUWARE_TESTS_DOCUMENT_FILE_SIZE_PREVIEW'),
-        'document_file_size' => (int) env('DOCUWARE_TESTS_DOCUMENT_FILE_SIZE'),
-        'document_count' => (int) env('DOCUWARE_TESTS_DOCUMENT_COUNT'),
-        'document_thumbnail_mime_type' => env('DOCUWARE_TESTS_DOCUMENT_THUMBNAIL_MIME_TYPE'),
-        'document_thumbnail_file_size' => (int) env('DOCUWARE_TESTS_DOCUMENT_THUMBNAIL_FILE_SIZE'),
-        'document_ids' => json_decode(env('DOCUWARE_TESTS_DOCUMENTS_IDS', '[]')),
-        'documents_file_size' => (int) env('DOCUWARE_TESTS_DOCUMENTS_FILE_SIZE'),
-        'field_name' => env('DOCUWARE_TESTS_FIELD_NAME'),
-        'field_name_2' => env('DOCUWARE_TESTS_FIELD_NAME_2'),
-    ],
 ];

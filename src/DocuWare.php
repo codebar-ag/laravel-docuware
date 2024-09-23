81b522025cbbbe3adb40aa77243b9b9de7f3992c
Sebastian Fix <sebastian.fix@codebar.ch>
2024-09-23 13:53:09 +0200

    Fixed OAuth-Bug resolves #170

--- src/DocuWare.php
+++ src/DocuWare.php
@@ -2,35 +2,8 @@
 
 namespace CodebarAg\DocuWare;
 
-use CodebarAg\DocuWare\DTO\Authentication\OAuth\RequestToken as RequestTokenDto;
-use CodebarAg\DocuWare\Requests\Authentication\OAuth\GetIdentityServiceConfiguration;
-use CodebarAg\DocuWare\Requests\Authentication\OAuth\GetResponsibleIdentityService;
-use CodebarAg\DocuWare\Requests\Authentication\OAuth\RequestTokenWithCredentials;
-
 class DocuWare
 {
-    public function getNewAuthenticationOAuthToken(
-        ?string $username = '',
-        ?string $password = '',
-        ?string $grantType = 'password',
-        ?string $clientId = 'docuware.platform.net.client'
-    ): RequestTokenDto {
-        $responsibleIdentityServiceResponse = (new GetResponsibleIdentityService)->send();
-
-        $identityServiceConfigurationResponse = (new GetIdentityServiceConfiguration(
-            identityServiceUrl: $responsibleIdentityServiceResponse->dto()->identityServiceUrl
-        ))->send();
-
-        $requestTokenResponse = (new RequestTokenWithCredentials(
-            tokenEndpoint: $identityServiceConfigurationResponse->dto()->tokenEndpoint,
-            clientId: $clientId,
-            username: $username,
-            password: $password,
-        ))->send();
-
-        return $requestTokenResponse->dto();
-    }
-
     public function searchRequestBuilder(): DocuWareSearchRequestBuilder
     {
         return new DocuWareSearchRequestBuilder;

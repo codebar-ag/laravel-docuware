6d4bbdfeebe81545dd34c887821cca03daccc01b
Sebastian Fix <sebastian.fix@codebar.ch>
2024-11-13 23:22:26 +0100

    WIP

--- src/Connectors/DocuWareConnector.php
+++ src/Connectors/DocuWareConnector.php
@@ -20,139 +20,141 @@ use Saloon\Http\Connector;
 
 class DocuWareConnector extends Connector
 {
-    public function __construct(
-        public ConfigWithCredentials|ConfigWithCredentialsTrustedUser $configuration
-    ) {}
+	public function __construct(
+		public ConfigWithCredentials|ConfigWithCredentialsTrustedUser $configuration
+	)
+	{
+	}
 
-    /**
-     * @throws \Exception
-     */
-    public function resolveBaseUrl(): string
-    {
-        return $this->configuration->url.'/DocuWare/Platform';
-    }
+	/**
+	 * @throws \Exception
+	 */
+	public function resolveBaseUrl(): string
+	{
+		return $this->configuration->url . '/DocuWare/Platform';
+	}
 
-    public function defaultHeaders(): array
-    {
-        return [
-            'Accept' => 'application/json',
-        ];
-    }
+	public function defaultHeaders(): array
+	{
+		return [
+			'Accept' => 'application/json',
+		];
+	}
 
-    public function defaultConfig(): array
-    {
-        return [
-            'timeout' => $this->configuration->requestTimeoutInSeconds,
-        ];
-    }
+	public function defaultConfig(): array
+	{
+		return [
+			'timeout' => $this->configuration->requestTimeoutInSeconds,
+		];
+	}
 
-    protected function defaultAuth(): TokenAuthenticator
-    {
-        return new TokenAuthenticator($this->getOrCreateNewOAuthToken());
-    }
+	protected function defaultAuth(): TokenAuthenticator
+	{
+		return new TokenAuthenticator($this->getOrCreateNewOAuthToken());
+	}
 
-    /**
-     * @throws InvalidArgumentException
-     * @throws \Exception
-     */
-    protected function getOrCreateNewOAuthToken(): string
-    {
-        $cache = Cache::store($this->configuration->cacheDriver);
+	/**
+	 * @throws InvalidArgumentException
+	 * @throws \Exception
+	 */
+	protected function getOrCreateNewOAuthToken(): string
+	{
+		$cache = Cache::store($this->configuration->cacheDriver);
 
-        $cacheKey = 'docuware.oauth.'.$this->configuration->identifier;
+		$cacheKey = 'docuware.oauth.' . $this->configuration->identifier;
 
-        // Check if the token exists in cache and return it if found
-        if ($cache->has($cacheKey)) {
-            $token = Crypt::decrypt($cache->get($cacheKey));
-            DocuWareOAuthLog::dispatch($this->configuration->url, $this->configuration->username, 'Token retrieved from cache');
+		// Check if the token exists in cache and return it if found
+		if ($cache->has($cacheKey)) {
+			$token = Crypt::decrypt($cache->get($cacheKey));
+			DocuWareOAuthLog::dispatch($this->configuration->url, $this->configuration->username, 'Token retrieved from cache');
 
-            return $token->accessToken;
-        }
+			return $token->accessToken;
+		}
 
-        // Handle token retrieval for ConfigWithCredentials
-        if ($this->configuration instanceof ConfigWithCredentials) {
-            $token = $this->getNewOAuthTokenWithCredentials();
-            DocuWareOAuthLog::dispatch($this->configuration->url, $this->configuration->username, 'Token retrieved from API');
-            $cache->put($cacheKey, Crypt::encrypt($token), $token->expiresIn - 60);
+		// Handle token retrieval for ConfigWithCredentials
+		if ($this->configuration instanceof ConfigWithCredentials) {
+			$token = $this->getNewOAuthTokenWithCredentials();
+			DocuWareOAuthLog::dispatch($this->configuration->url, $this->configuration->username, 'Token retrieved from API');
+			$cache->put($cacheKey, Crypt::encrypt($token), $token->expiresIn - 60);
 
-            return $token->accessToken;
-        }
+			return $token->accessToken;
+		}
 
-        // Handle token retrieval for ConfigWithCredentialsTrustedUser
-        if ($this->configuration instanceof ConfigWithCredentialsTrustedUser) {
-            $token = $this->getNewOAuthTokenWithCredentialsTrustedUser();
-            DocuWareOAuthLog::dispatch($this->configuration->url, $this->configuration->username, 'Token retrieved from API');
-            $cache->put($cacheKey, Crypt::encrypt($token), $token->expiresIn - 60);
+		// Handle token retrieval for ConfigWithCredentialsTrustedUser
+		if ($this->configuration instanceof ConfigWithCredentialsTrustedUser) {
+			$token = $this->getNewOAuthTokenWithCredentialsTrustedUser();
+			DocuWareOAuthLog::dispatch($this->configuration->url, $this->configuration->username, 'Token retrieved from API');
+			$cache->put($cacheKey, Crypt::encrypt($token), $token->expiresIn - 60);
 
-            return $token->accessToken;
-        }
-    }
+			return $token->accessToken;
+		}
+	}
 
-    protected function getAuthenticationTokenEndpoint(): IdentityServiceConfiguration
-    {
-        $responsibleIdentityServiceResponse = (new GetResponsibleIdentityService($this->configuration->url))->send();
+	protected function getAuthenticationTokenEndpoint(): IdentityServiceConfiguration
+	{
+		$responsibleIdentityServiceResponse = (new GetResponsibleIdentityService($this->configuration->url))->send();
 
-        $identityServiceConfigurationResponse = (new GetIdentityServiceConfiguration(
-            identityServiceUrl: $responsibleIdentityServiceResponse->dto()->identityServiceUrl
-        ))->send();
+		$identityServiceConfigurationResponse = (new GetIdentityServiceConfiguration(
+			identityServiceUrl: $responsibleIdentityServiceResponse->dto()->identityServiceUrl
+		))->send();
 
-        return $identityServiceConfigurationResponse->dto();
-    }
+		return $identityServiceConfigurationResponse->dto();
+	}
 
-    /**
-     * @throws \Throwable
-     * @throws \JsonException
-     */
-    protected function getNewOAuthTokenWithCredentials(): RequestTokenDto
-    {
-        $requestTokenResponse = (new RequestTokenWithCredentials(
-            tokenEndpoint: $this->getAuthenticationTokenEndpoint()->tokenEndpoint,
-            clientId: $this->configuration->clientId,
-            scope: $this->configuration->scope,
-            username: $this->configuration->username,
-            password: $this->configuration->password,
-        ))->send();
+	/**
+	 * @throws \Throwable
+	 * @throws \JsonException
+	 */
+	protected function getNewOAuthTokenWithCredentials(): RequestTokenDto
+	{
+		$requestTokenResponse = (new RequestTokenWithCredentials(
+			tokenEndpoint: $this->getAuthenticationTokenEndpoint()->tokenEndpoint,
+			clientId: $this->configuration->clientId,
+			scope: $this->configuration->scope,
+			username: $this->configuration->username,
+			password: $this->configuration->password,
+		))->send();
 
-        throw_if(
-            $requestTokenResponse->failed(),
-            trim(preg_replace('/\s\s+/', ' ', Arr::get(
-                array: $requestTokenResponse->json(),
-                key: 'error_description',
-                default: $requestTokenResponse->body()
-            )))
-        );
+		throw_if(
+			$requestTokenResponse->failed(),
+			trim(preg_replace('/\s\s+/', ' ', Arr::get(
+				array: $requestTokenResponse->json(),
+				key: 'error_description',
+				default: $requestTokenResponse->body()
+			)))
+		);
 
-        throw_if($requestTokenResponse->dto() == null, 'Token response is null');
+		throw_if($requestTokenResponse->dto() == null, 'Token response is null');
 
-        return $requestTokenResponse->dto();
-    }
+		return $requestTokenResponse->dto();
+	}
 
-    /**
-     * @throws \Throwable
-     * @throws \JsonException
-     */
-    protected function getNewOAuthTokenWithCredentialsTrustedUser(): RequestTokenDto
-    {
-        $requestTokenResponse = (new RequestTokenWithCredentialsTrustedUser(
-            tokenEndpoint: $this->getAuthenticationTokenEndpoint()->tokenEndpoint,
-            clientId: $this->configuration->clientId,
-            scope: $this->configuration->scope,
-            username: $this->configuration->username,
-            password: $this->configuration->password,
-            impersonateName: $this->configuration->impersonatedUsername,
-        ))->send();
+	/**
+	 * @throws \Throwable
+	 * @throws \JsonException
+	 */
+	protected function getNewOAuthTokenWithCredentialsTrustedUser(): RequestTokenDto
+	{
+		$requestTokenResponse = (new RequestTokenWithCredentialsTrustedUser(
+			tokenEndpoint: $this->getAuthenticationTokenEndpoint()->tokenEndpoint,
+			clientId: $this->configuration->clientId,
+			scope: $this->configuration->scope,
+			username: $this->configuration->username,
+			password: $this->configuration->password,
+			impersonateName: $this->configuration->impersonatedUsername,
+		))->send();
 
-        throw_if(
-            $requestTokenResponse->failed(),
-            trim(preg_replace('/\s\s+/', ' ', Arr::get(
-                array: $requestTokenResponse->json(),
-                key: 'error_description',
-                default: $requestTokenResponse->body()
-            )))
-        );
+		throw_if(
+			$requestTokenResponse->failed(),
+			trim(preg_replace('/\s\s+/', ' ', Arr::get(
+				array: $requestTokenResponse->json(),
+				key: 'error_description',
+				default: $requestTokenResponse->body()
+			)))
+		);
 
-        throw_if($requestTokenResponse->dto() == null, 'Token response is null');
+		throw_if($requestTokenResponse->dto() == null, 'Token response is null');
 
-        return $requestTokenResponse->dto();
-    }
+		return $requestTokenResponse->dto();
+	}
 }

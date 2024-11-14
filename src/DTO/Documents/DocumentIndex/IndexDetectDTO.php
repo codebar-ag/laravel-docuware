c50ce7dc43b2e9cd6d81870f5d32b5b223509ddf
Sebastian Fix <sebastian.fix@codebar.ch>
2024-11-14 02:27:04 +0100

    WIP

---
+++ src/DTO/Documents/DocumentIndex/IndexDetectDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CodebarAg\DocuWare\DTO\Documents\DocumentIndex;
+
+use Illuminate\Support\Carbon;
+
+class IndexDetectDTO
+{
+    public function __construct(
+        public string $name,
+        public mixed $value,
+    ) {}
+
+    public static function make(string $name, mixed $value)
+    {
+        return match (true) {
+            is_string($value) => IndexTextDTO::make($name, $value),
+            is_int($value) => IndexNumericDTO::make($name, $value),
+            is_float($value) => IndexDecimalDTO::make($name, $value),
+            $value instanceof Carbon => IndexDateTimeDTO::make($name, $value),
+            default => null,
+        };
+    }
+}
